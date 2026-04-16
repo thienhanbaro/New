f3f96ba310d88366eadf22ae0264a5fac6e40811
thienhanbaro <lethiennhanvip.vn@gmail.com>
2026-04-17 05:48:59 +0700

    Create ToolBoxAppDelegate.m

---
+++ ToolBoxAppDelegate.m
@@ -0,0 +1,13 @@
+#import "ToolBoxAppDelegate.h"
+#import "ToolBoxRootViewController.h"
+
+@implementation ToolBoxAppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    ToolBoxRootViewController *rootVC = [[ToolBoxRootViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:rootVC];
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
